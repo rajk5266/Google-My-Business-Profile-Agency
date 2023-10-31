26e6aab57e871d05871727ecfb24383a4623ba75
rajk5266 <kushwaharaj903@gmail.com>
2023-10-31 18:03:30 +0530

    php backend

---
+++ index.php
@@ -0,0 +1,427 @@
+<!DOCTYPE html>
+<html lang="en">
+  <!-- HEADER  -->
+  <?php include "header.php"; ?>
+  <!-- HEADER  -->
+  <body>
+  
+<!-- NAVBAR  -->
+<?php 
+ include "navbar.php"
+?>
+<!-- NAVBAR  -->
+    <!-- HERO BANNER  -->
+
+    <div class="container-fluid">
+      <div class="hero-banner">
+        <div class="row">
+          <!-- <div class="col-col-2"></div> -->
+          <div class="col-sm-10 col-md-6 col-lg-6" style="padding: 5%">
+            <div class="hero-banner-image">
+              <img src="image/hero-banner.jpeg" alt="" />
+            </div>
+          </div>
+          <div class="col col-sm-12 col-md-4 col-lg-3" style="padding: 5%">
+            <div class="hero-banner-description">
+              <h1>Google My Business Profile Agency</h1>
+              <h3>Lorem ipsum dolor sit amet.</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- HERO BANNER  -->
+
+    <!-- ABOUT SECTION  -->
+
+    <div class="container-fluid">
+      <div class="main-content">
+        <div class="row">
+          <div class="col col-12">
+            <h1>Google My Business</h1>
+          </div>
+          <div class="col col-12">
+            <h3>Lorem, ipsum dolor.</h3>
+          </div>
+
+          <div class="col col-12">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Perferendis, autem hic? Quod dolor dignissimos tempora modi,
+              blanditiis asperiores ea aliquam.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- ABOUT SECTION  -->
+
+    <!-- CLIENT LOGOS SECTION   -->
+    <div class="container">
+		<h1>Our Clients</h1>
+		<div class="client">
+		<ul>
+			<li>
+				<img src="https://cdn.pixabay.com/photo/2021/08/27/06/25/wolf-6577886__340.png" alt="client1">
+				<img src="https://cdn.pixabay.com/photo/2017/01/31/23/42/animal-2028258__340.png" alt="client1">
+			</li>
+			<li>
+				<img src="https://cdn.pixabay.com/photo/2020/04/24/10/56/music-5086395__340.png" alt="client2">
+				<img src="https://cdn.pixabay.com/photo/2018/04/11/20/12/silhouette-3311639__340.png" alt="client2">
+			</li>
+			<li>
+				<img src="https://cdn.pixabay.com/photo/2017/02/01/11/30/cap-2029798__340.png" alt="client3">
+				<img src="https://cdn.pixabay.com/photo/2014/04/03/10/02/graduation-309661__340.png" alt="client3">
+			</li>
+			<li>
+				<img src="https://cdn.pixabay.com/photo/2020/06/24/05/23/tree-5334773__340.png" alt="client4">
+				<img src="https://cdn.pixabay.com/photo/2020/06/24/05/29/tree-5334823__340.png" alt="client4">
+			</li>
+		</ul>
+	</div>
+	<!-- <div class="credit">Made with <span style="color:tomato">❤</span> by <a  href="https://www.learningrobo.com/">Learning Robo</a></div> -->
+	</div>
+
+    <!-- CLIENT LOGOS SECTION   -->
+
+    <div class="services">
+      <div class="container-fluid our-services">
+        <div class="row our-services-title">
+          <h1>*********</h1>
+        </div>
+        <div class="row services-row">
+
+            <div class="col col-sm-10 col-md-10 col-lg-5">
+                <div class="row">
+                    <div class="col col-10 service-image">
+                        <img
+                          src="image/services.jpeg"
+                          alt="Image Description"
+                          class="img-fluid"
+                        />
+                      </div>
+                      <div class="col col-10 service-description">
+                        <h3>Google Business Profile Management</h3>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                          Provident eaque iste perferendis commodi doloremque inventore
+                          corporis a quos dolore eius.
+                        </p>
+                    </div>
+                </div>  
+            </div>
+
+            <div class="col col-sm-10 col-md-10 col-lg-5">
+                <div class="row">
+                    <div class="col col-10 service-image">
+                        <img
+                          src="image/services.jpeg"
+                          alt="Image Description"
+                          class="img-fluid"
+                        />
+                      </div>
+                      <div class="col col-10 service-description">
+                        <h3>Google Business Profile Management</h3>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                          Provident eaque iste perferendis commodi doloremque inventore
+                          corporis a quos dolore eius.
+                        </p>
+                    </div>
+                </div>  
+            </div>
+
+            <div class="col col-sm-10 col-md-10 col-lg-5">
+                <div class="row">
+                    <div class="col col-10 service-image">
+                        <img
+                          src="image/services.jpeg"
+                          alt="Image Description"
+                          class="img-fluid"
+                        />
+                      </div>
+                      <div class="col col-10 service-description">
+                        <h3>Google Business Profile Management</h3>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                          Provident eaque iste perferendis commodi doloremque inventore
+                          corporis a quos dolore eius.
+                        </p>
+                    </div>
+                </div>  
+            </div>
+
+            <div class="col col-sm-10 col-md-10 col-lg-5">
+                <div class="row">
+                    <div class="col col-10 service-image">
+                        <img
+                          src="image/services.jpeg"
+                          alt="Image Description"
+                          class="img-fluid"
+                        />
+                      </div>
+                      <div class="col col-10 service-description">
+                        <h3>Google Business Profile Management</h3>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                          Provident eaque iste perferendis commodi doloremque inventore
+                          corporis a quos dolore eius.
+                        </p>
+                    </div>
+                </div>  
+            </div>
+
+            <div class="col col-sm-10 col-md-10 col-lg-5">
+                <div class="row">
+                    <div class="col col-10 service-image">
+                        <img
+                          src="image/services.jpeg"
+                          alt="Image Description"
+                          class="img-fluid"
+                        />
+                      </div>
+                      <div class="col col-10 service-description">
+                        <h3>Brand Identity </h3>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                          Provident eaque iste perferendis commodi doloremque inventore
+                          corporis a quos dolore eius.
+                        </p>
+                    </div>
+                </div>  
+            </div>
+        </div>
+
+      </div>
+    </div>
+
+    <!-- OUR WORKS  -->
+    <div class="conatiner gallery-container">
+      <div class="row">
+        <div class="col col-12 title">
+          <h1>Our Work is for...</h1>
+        </div>
+        <div class="col col-12">
+          <!-- <h1>Healthcare </h1> -->
+        </div>
+      </div>
+      <section id="gallery">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-4 mb-4">
+              <div class="card">
+                <img
+                  src="https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60"
+                  alt=""
+                  class="card-img-top"
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Small Business</h5>
+                  <p class="card-text">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut
+                    eum similique repellat a laborum, rerum voluptates ipsam eos
+                    quo tempore iusto dolore modi dolorum in pariatur. Incidunt
+                    repellendus praesentium quae!
+                  </p>
+                  <!-- <a href="" class="btn btn-outline-success btn-sm"
+                    >Read More</a -->
+                  
+                  <!-- <a href="" class="btn btn-outline-danger btn-sm"
+                  ><i class="far fa-heart"></i
+                ></a> -->
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+              <div class="card">
+                <img
+                  src="https://images.unsplash.com/photo-1516214104703-d870798883c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60"
+                  alt=""
+                  class="card-img-top"
+                />
+                <div class="card-body">
+                  <h5 class="card-title">
+                    something
+                  </h5>
+                  <p class="card-text">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut
+                    eum similique repellat a laborum, rerum voluptates ipsam eos
+                    quo tempore iusto dolore modi dolorum in pariatur. Incidunt
+                    repellendus praesentium quae!
+                  </p>
+                  <!-- <a href="" class="btn btn-outline-success btn-sm"
+                    >Read More</a -->
+                  
+                  <!-- <a href="" class="btn btn-outline-danger btn-sm"
+                  ><i class="far fa-heart"></i
+                ></a> -->
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+              <div class="card">
+                <img
+                  src="https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60"
+                  alt=""
+                  class="card-img-top"
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Institute / Classess</h5>
+                  <p class="card-text">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut
+                    eum similique repellat a laborum, rerum voluptates ipsam eos
+                    quo tempore iusto dolore modi dolorum in pariatur. Incidunt
+                    repellendus praesentium quae!
+                  </p>
+                  <!-- <a href="" class="btn btn-outline-success btn-sm"
+                    >Read More</a -->
+                  
+                  <!-- <a href="" class="btn btn-outline-danger btn-sm"
+                  ><i class="far fa-heart"></i
+                ></a> -->
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-4 mb-4">
+              <div class="card">
+                <img
+                  src="https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60"
+                  alt=""
+                  class="card-img-top"
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Event Planners</h5>
+                  <p class="card-text">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut
+                    eum similique repellat a laborum, rerum voluptates ipsam eos
+                    quo tempore iusto dolore modi dolorum in pariatur. Incidunt
+                    repellendus praesentium quae!
+                  </p>
+                  <!-- <a href="" class="btn btn-outline-success btn-sm"
+                    >Read More</a -->
+                  
+                   <!-- <a href="" class="btn btn-outline-danger btn-sm"
+                  ><i class="far fa-heart"></i
+                ></a>  -->
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+
+    <!-- OUR WORKS  -->
+
+    <!-- WHO CAN CONNECT US  -->
+
+    <!-- WHO CAN CONNECT US  -->
+
+    <!-- TESTIMONIALS  -->
+    <div class="container">
+        <div class="row testimonial-title">
+            <div class="col col-12">What Clients think about us</div>
+        </div>
+    </div>
+
+    <div class="outerdiv">
+        
+        <div class="innerdiv">
+          <!-- div1 -->
+          <div class="div1 eachdiv">
+            <div class="userdetails">
+              <div class="imgbox">
+                <img src="https://raw.githubusercontent.com/RahulSahOfficial/testimonials_grid_section/5532c958b7d3c9b910a216b198fdd21c73112d84/images/image-daniel.jpg" alt="">
+              </div>
+              <div class="detbox">
+                <p class="name">Daniel Clifford</p>
+                <p class="designation">Verified Graduate</p>
+              </div>
+            </div>
+            <div class="review">
+              <h4>I received a job offer mid-course, and the subjects I learned were current, if not more so, in the company I joined. I honestly feel I got every penny’s worth.</h4>
+            <p>“ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a transition and have heard some people who had an amazing experience here. I signed up for the free intro course and found it incredibly fun! I enrolled shortly thereafter. The next 12 weeks was the best - and most grueling - time of my life. Since completing the course, I’ve successfully switched careers, working as a Software Engineer at a VR startup. ”</p>
+            </div>
+          </div>
+          <!-- div2 -->
+          <div class="div2 eachdiv">
+            <div class="userdetails">
+              <div class="imgbox">
+                <img src="https://raw.githubusercontent.com/RahulSahOfficial/testimonials_grid_section/5532c958b7d3c9b910a216b198fdd21c73112d84/images/image-jonathan.jpg" alt="">
+              </div>
+              <div class="detbox">
+                <p class="name">Jonathan Walters</p>
+                <p class="designation">Verified Graduate</p>
+              </div>
+            </div>
+            <div class="review">
+              <h4>The team was very supportive and kept me motivated</h4>
+            <p>“ I started as a total newbie with virtually no coding skills. I now work as a mobile engineer for a big company. This was one of the best investments I’ve made in myself. ”</p>
+            </div>
+          </div>
+          <!-- div3 -->
+          <div class="div3 eachdiv">
+            <div class="userdetails">
+              <div class="imgbox">
+                <img src="https://raw.githubusercontent.com/RahulSahOfficial/testimonials_grid_section/5532c958b7d3c9b910a216b198fdd21c73112d84/images/image-kira.jpg" alt="">
+              </div>
+              <div class="detbox">
+                <p class="name dark">Kira Whittle</p>
+                <p class="designation dark">Verified Graduate</p>
+              </div>
+            </div>
+            <div class="review dark">
+              <h4>Such a life-changing experience. Highly recommended!</h4>
+              <p>“ Before joining the bootcamp, I’ve never written a line of code. I needed some structure from professionals who can help me learn programming step by step. I was encouraged to enroll by a former student of theirs who can only say wonderful things about the program. The entire curriculum and staff did not disappoint. They were very hands-on and I never had to wait long for assistance. The agile team project, in particular, was outstanding. It took my learning to the next level in a way that no tutorial could ever have. In fact, I’ve often referred to it during interviews as an example of experience. It certainly helped me land a job as a full-stack 100% recommend! ”</p>
+            </div>
+          </div>
+          <!-- div4 -->
+          <div class="div4 eachdiv">
+            <div class="userdetails">
+              <div class="imgbox">
+                <img src="https://raw.githubusercontent.com/RahulSahOfficial/testimonials_grid_section/5532c958b7d3c9b910a216b198fdd21c73112d84/images/image-jeanette.jpg" alt="">
+              </div>
+              <div class="detbox">
+                <p class="name dark">Jeanette Harmon</p>
+                <p class="designation dark">Verified Graduate</p>
+              </div>
+            </div>
+            <div class="review dark">
+              <h4>An overall wonderful and rewarding experience</h4>
+            <p>“ Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living while doing something I love. ”</p>
+            </div>
+          </div>
+          <!-- div5 -->
+          <div class="div5 eachdiv">
+            <div class="userdetails">
+              <div class="imgbox">
+                <img src="https://raw.githubusercontent.com/RahulSahOfficial/testimonials_grid_section/5532c958b7d3c9b910a216b198fdd21c73112d84/images/image-patrick.jpg" alt="">
+              </div>
+              <div class="detbox">
+                <p class="name">Patrick Abrams</p>
+                <p class="designation">Verified Graduate</p>
+              </div>
+            </div>
+            <div class="review">
+              <h4>Awesome teaching support from TAs who did the bootcamp themselves. Getting guidance from them and learning from their experiences was easy.</h4>
+            <p>“ The staff seem genuinely concerned about my progress which I find really refreshing. The program gave me the confidence necessary to be able to go out in the world and present myself as a capable junior developer. The standard is above the rest. You will get the personal attention you need from an incredible community of smart and amazing people. ”</p>
+            </div>
+          </div>
+        </div>
+      </div>
+     
+    <!-- TESTIMONIALS  -->
+
+    <!-- FOOTER  -->
+
+   <?php include "footer.php"; ?>
+    <!-- FOOTER  -->
+
+    <!-- SERVICES  -->
+
+    <!-- SERVICES  -->
+
+    <script src="main.js"></script>
+  </body>
+</html>
